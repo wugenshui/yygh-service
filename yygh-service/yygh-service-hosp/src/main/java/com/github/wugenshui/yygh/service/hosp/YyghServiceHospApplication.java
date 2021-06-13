d39265951f77358fe7cdba1d1a0dc485a0800da8
chenbo <963838470@qq.com>
2021-06-13 22:03:24 +0800

    代码生成器 + 脚本 + 结构微调

---
+++ yygh-service/yygh-service-hosp/src/main/java/com/github/wugenshui/yygh/service/hosp/YyghServiceHospApplication.java
@@ -0,0 +1,15 @@
+package com.github.wugenshui.yygh.service.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2021-06-13
+ */
+@SpringBootApplication
+public class YyghServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YyghServiceHospApplication.class, args);
+    }
+}
